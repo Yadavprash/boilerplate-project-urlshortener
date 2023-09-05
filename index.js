08be569bebc0bfe1009e0e5fe4ab6d45bcf4ff0a
Yadavprash <py8986412056@gmail.com>
2023-09-05 16:15:10 +0530

    try: path using dirname

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 app.post('/api/shorturl',(req,res)=>{
